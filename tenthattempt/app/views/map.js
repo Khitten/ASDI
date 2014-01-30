7474dd9626c807d450fc3d8f53f7ae476d25bcd0
Cassandra <cmorath@fullsail.edu>
2014-01-30 14:36:22 -0500

    Trying to follow along

--- tenthattempt/app/views/map.js
+++ tenthattempt/app/views/map.js
@@ -1,5 +1,9 @@
 function(doc) {
-  if (doc.created_at) {
-    emit(doc.created_at, doc);
+  if (doc._id.substring(0,8) === "program:") {
+    emit(doc._id.subtr(8), {
+    	"title" : doc.title,
+    	"acronym" : doc.acronym,
+    	"months" : doc.months
+    });
   }
 };
