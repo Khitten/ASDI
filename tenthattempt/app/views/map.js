36ea4b743784943f3dc801605a34f1d0451f9287
Cassandra <cmorath@fullsail.edu>
2014-01-30 18:08:37 -0500

    Last try at getting this running correctly

--- tenthattempt/app/views/map.js
+++
@@ -1,9 +0,0 @@
-function(doc) {
-  if (doc._id.substring(0,8) === "program:") {
-    emit(doc._id.subtr(8), {
-    	"title" : doc.title,
-    	"acronym" : doc.acronym,
-    	"months" : doc.months
-    });
-  }
-};
--- tenthattempt/main.js
+++
@@ -1,5 +0,0 @@
-// JavaScript Document
-//  Cassandra Morath
-//	ASDI 1310
-//  10/28/13
-//GH-Pages Added
